3125c121fd0b755026237745c50f9b1d0e520f28
PHW <pedrolopeshls99@gmail.com>
2020-04-06 23:39:48 -0300

    start preview

--- help-delivery/src/pages/Preview/index.js
+++ help-delivery/src/pages/Preview/index.js
@@ -1,19 +1,35 @@
 import React from 'react'
+import './style.scss'
 import { Link } from 'react-router-dom'
+import logoImg from '../../assets/logo.png'
 
 export default class Preview extends React.Component{
      render(){
         return(
             <div>
-                <h1>Preview vai aqui</h1>
+
+
+                <div className="container-sides">
+
+                    <div className="container-left ac">
+                        <img className="display-b ac" src={logoImg} alt="delivery"/>
+                    </div>
+
+                    <div className="container-rigth ac">
+                        <div className="card">
+                            <h1 className="title">Preview vai aqui</h1>
+
+                        </div>
+                    </div>
+
+                </div>
+
+
                 <Link to="/Login">
-                    <button>login</button>
+                    <button className="btn">login</button>
                 </Link>
-{/* 
-                <Link className="back-link" to="/register">
-                    <FiLogIn size={16} color="#E02041"/>
-                    Não tenho cadastro
-                </Link> */}
+
+
             </div>
         )
      }
